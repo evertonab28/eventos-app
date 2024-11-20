cf941d7ef02a757bee60e276afb87c95519e747c
Everton <evertonab.dev@gmail.com>
2024-11-19 20:57:40 -0400

    creation of evento and convidado models

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Pagina from "@/components/template/Pagina";
+import { Evento } from "@/core";
 
 export default function Home() {
+  const e: Partial<Evento> = {}
   return (
     <Pagina>
       <div className="text-white">Inicio</div>
