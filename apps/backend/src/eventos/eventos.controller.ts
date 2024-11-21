f28cb87f11c241a67ddc649298b4bd154fe127cc
Everton <evertonab.dev@gmail.com>
2024-11-21 14:03:07 -0400

    creation of search and validation of evento in evento controller

--- apps/backend/src/eventos/eventos.controller.ts
+++ apps/backend/src/eventos/eventos.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { Data, Evento, eventos } from 'core';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Data, Evento, eventos, Id } from 'core';
 
 @Controller('eventos')
 export class EventosController {
@@ -8,7 +8,25 @@ export class EventosController {
     return eventos.map(this.serializar);
   }
 
+  @Get(':idOuAlias')
+  async buscarEvento(@Param('idOuAlias') idOuAlias: string) {
+    if (Id.valido(idOuAlias)) {
+      return this.serializar(eventos.find((evento) => evento.id === idOuAlias));
+    } else {
+      return this.serializar(
+        eventos.find((evento) => evento.alias === idOuAlias),
+      );
+    }
+  }
+
+  @Get('validar/:alias/:id')
+  async validarAlias(@Param('alias') alias: string, @Param('id') id: string) {
+    const evento = eventos.find((evento) => evento.alias === alias);
+    return { valido: !evento || evento.id === id };
+  }
+
   private serializar(evento: Evento) {
+    if (!evento) return null;
     return {
       ...evento,
       data: Data.formatar(evento.data),
